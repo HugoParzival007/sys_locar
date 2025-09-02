7e96225f8cbfe64e2ebe1840fad6ed679c59c11c
HugoParzival007 <90792051+HugoParzival007@users.noreply.github.com>
2025-09-02 09:33:26 -0300

    construtores

---
+++ src/Model/Funcionario.java
@@ -0,0 +1,17 @@
+package Model;
+
+public class Funcionario {
+    private long id;
+
+    public Funcionario(long id) {
+        this.id = id;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
---
+++ src/Model/Ocorrencia.java
@@ -0,0 +1,17 @@
+package Model;
+
+public class Ocorrencia {
+    private long id;
+
+    public Ocorrencia(long id) {
+        this.id = id;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
